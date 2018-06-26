7439a0cc4e88052b3b1fba027b5d4e980413c10a
20161104612 <a20161104612@mac23.local>
2018-06-26 11:15:51 +0800

    Initial Commit

---
+++ 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2
+//
+//  Created by 20161104612 on 18/6/26.
+//  Copyright © 2018年 20161104612. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
